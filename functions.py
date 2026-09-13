677d6bb5b3b8a07c754c04f7b3b8ce79cef86f50
rahmatullohsotiboldiev9-tech <rahmatullohsotiboldiev9@gmail.com>
2026-09-13 11:51:19 +0900

    feat: scope & priority tushunchasi

--- functions.py
+++ functions.py
@@ -45,3 +45,18 @@ print("result3:", result3)
 
 result4 = give_greet("John")
 print("result4:", result4)
+
+
+print("===== Scope =====")
+b = 100
+
+# DEFINE
+
+
+def calculate(a,):
+    c = a * b
+    print(f"the c value: {c}")
+
+
+# CALL
+calculate(5)
