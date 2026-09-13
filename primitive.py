e88454ba22164b315d5d475cd56713a80a368d02
rahmatullohsotiboldiev9-tech <rahmatullohsotiboldiev9@gmail.com>
2026-09-13 10:48:48 +0900

    feat: learn python boolean

---
+++ primitive.py
@@ -0,0 +1,48 @@
+print("===== number =====")
+# in JAVA, variable is a name storage location!
+# in Python, variable is named reference!
+
+count = 100
+count_type = type(count)
+print(f"the count: {count} and type: {count_type}")
+
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
+print(result1, result2)
+
+print("===== string =====")
+# methods: title(), upper(), lower(), capitalize(), replace(), find().
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result (1): {result}")
+
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("FullStack", "Masteclass")
+print(f"the result (4): {result}")
+
+
+print("===== boolean =====")
+# function > typee () input() bool() int() str() float() list() dict() set() tuple()
+
+y = input("Give your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY vs FALSY value
+# TRUTHY: True 100 -100 "MIT"
+# FALSY: False 0 "" None
+
+test_falsy = "" or False or None or 0
+print("test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))
